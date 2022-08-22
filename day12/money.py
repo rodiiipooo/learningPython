c562af7fdcbffd7fa174bdd703724d39f0461a92
Rod! <110260818+rodiiipooo@users.noreply.github.com>
2022-08-21 22:16:30 -0500

    Add files via upload
    Trying to make a function that will allow one to better study their income from only a yearly salary or hourly wage offer.

---
+++ day12/money.py
@@ -0,0 +1,30 @@
+
+year = 'year'
+
+
+class pay():
+
+    def year_summary():
+        pass
+
+    def hour_summary():
+        pass
+
+
+class money():
+    # def
+    if yearOrHour == "year":
+        pay = ''
+        while pay == '':
+            pay = int(input("what is your yearly salary?: "))
+        print(year_summary(pay))
+
+    else:
+        pay = ''
+        while pay == '':
+            pay = int(input("what is your yearly salary?: "))
+        print(year_summary(pay))
+
+
+money(year)
+# thinking about how to organize my function, unsure of wether or not to use classes or to use one function.
