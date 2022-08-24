15d29c29ce620fae6351ed1542de4e4658cd9af9
Rod! <110260818+rodiiipooo@users.noreply.github.com>
2022-08-24 17:54:18 -0500

    Add files via upload
    I will be working on  a tax preparation program.

---
+++ taxes/taxes.py
@@ -0,0 +1,14 @@
+class Money():
+
+    def analysis():
+        #calls both income() and costs() functions for an analysis
+        pass
+
+    def income():
+        input("What is your hourly wage or yearly salary?: ")
+        pass
+
+    def costs():
+        pass
+
+        
